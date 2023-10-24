9107d9b1de229bafa7d0f60551fe939a1892690f
Alopsis <zaxo.moulager@gmail.com>
2023-10-24 08:58:21 +0200

    Soulignage en rouge

--- index.php
+++ index.php
@@ -30,6 +30,8 @@ if (isset($_POST['contact-titre']) && isset($_POST['contact-mail']) && isset($_P
 <html lang="fr">
 
 <head>
+    <link rel="stylesheet" href="./style.css">
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -38,102 +40,177 @@ if (isset($_POST['contact-titre']) && isset($_POST['contact-mail']) && isset($_P
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/4b3a8b9333.js" crossorigin="anonymous"></script>
-    <script src="./script.js"></script>
     <link rel="shortcut icon" href="./flavicon-puce.png" type="images/x-icon" />
-    <link rel="stylesheet" href="./style.css">
+
+    <!-- Balises de referencements -->
+    <meta name="description" content="Portfolio personnel de Sinclair Moulager afin de présenter ses compétences et acquis pour la vie profesionnelle.">
+    <meta name="keywords" content="portfolio , cv , presentation , compétences , developpeur , developpeur-web , informatique">
+    <meta name="robots" content="index,follow">
     <title>Sinclair Moulager | Portfolio</title>
 </head>
 
 <body>
     <header>
         <ul class="head-ul">
-            <li class="head-list"><a class="head-a" href="#presentation">Presentation</a></li>
-            <li class="head-list"><a class="head-a" href="#competence">Competences</a></li>
+            <li class="head-list"><a class="head-a" href="#presentation">Présentation</a></li>
+            <li class="head-list"><a class="head-a" href="#competence">Compétences</a></li>
             <li class="head-list"><a class="head-a" href="#outils">Outils</a></li>
             <li class="head-list"><a class="head-a" href="#reseaux">Réseaux</a></li>
-            <li class="head-list"><a class="head-a" href="#contact">Contact</a></li>
+            <li class="head-list"><a href="#projet" class="head-a">Projets</a></li>
+            <li class="head-list"><a href="#contact" class="head-a">Contact</a></li>
         </ul>
     </header>
     <main>
         <section class="top">
             <div>
                 <h2 class="top-nom">Sinclair Moulager</h2>
-                <h3 class="top-role">Developpeur Informatique</h3>
+                <h3 class="top-role">Développeur Informatique</h3>
                 <h5 class="top-acc">Toujours prêt à coder pour vous !</h5>
-                <img class="top-image" src="./Sinclair_Moulager.jpg" alt="Photo de Sinclair Moulager"><br>
-                <img class="top-scm" src="./cloud.png" alt="">
-            </div>
+                <div class="wrapper">
+                    <img class="top-image" src="./Sinclair_Moulager.jpg" alt="Photo de Sinclair Moulager"><br>
+                    <img class="top-scm" src="./cloud.png" alt="">
+                </div>
         </section>
         <section class="info" id="presentation">
             <div class="info-div">
                 <h3 class="info-propos">À propos de moi</h3>
-                <p class="info-para">Actuellement en 2ème année de licence informatique a l'université Jean Monnet a
-                    Saint-Etienne. Je donne une importante considération a l'informatique et m'interesse a toutes les
-                    nouvelles decouvertes de ce domaine. De plus, je vais continuer l'année prochaine avec une 3ème année
-                    en Alternance.</p>
+                <hr class="hr">
+                <p class="info-para">Actuellement en 2ème année de licence informatique à l'université Jean Monnet de
+                    Saint-Etienne. Je donne une importante considération à l'informatique et m'intéresse à toutes les
+                    nouvelles découvertes de ce domaine. Je trouve l'informatique fascinant puisque ce domaine n'a jamais fini de nous surprendre et chaque projet est une nouvelle aventure a faire seul ou en groupe.</p>
                 <h3 class="info-contact">Me connaitre davantage</h3>
                 <a class="info-cv" href="./Sinclair_MOULAGER-CV.pdf" target="_blank">Voici mon CV</a>
             </div>
         </section>
-        <section class="comp" id="competence">
-            <div>
-                <h2 class="comp-titre">Mes competences</h2>
-                <div class="comp-web">
-                    <div class="comp-web-container">
-                        <div class="comp-front-container">
-                            <h4 class="comp-sous-titre">Front-End</h4>
-                            <div class="comp-img front">
-                                <div>
-                                    <img class="comp-image" src="./images/html.png" alt="">
-                                    <figcaption>Html</figcaption>
-                                </div>
-                                <div>
-                                    <img class="comp-image" src="./images/css.png" alt="">
-                                    <figcaption>Css</figcaption>
-                                </div>
-                                <div>
-                                    <img class="comp-image" src="./images/javascript.png" alt="">
-                                    <figure>JavaScript</figure>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="comp-front-container">
-                            <h4 class="comp-sous-titre">Back-End</h4>
-                            <div class="comp-img back">
-                                <div>
-                                    <img class="comp-image" src="./images/php.png" alt="">
-                                    <figcaption>Php</figcaption>
-                                </div>
-                                <div>
-                                    <img class="comp-image" src="./images/sql.png" alt="">
-                                    <figcaption>Sql</figcaption>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="comp-web-container">
-                    <div class="comp-front-container">
-                        <h4 class="comp-sous-titre">Autres</h4>
-                        <div class="comp-img">
-                            <div>
-                                <img class="comp-image" src="./images/c.png" alt="">
-                                <figcaption>C</figcaption>
-                            </div>
-                            <div>
-                                <img class="comp-image" src="./images/LaTeX.png" alt="">
-                                <figcaption>LaTeX</figcaption>
-                            </div>
 
-                        </div>
+        <section class="comp-refonte">
+            <p class="comp-title">Front</p>
+
+            <div class="comp-refonte-container">
+
+                <div class="container-box slide-in">
+                    <p class="box-title">HTML</p>
+
+                    <div class="box">
+
+                        <svg>
+                            <circle data-value="85" class="circle-comp" cx="70px" cy="70px" r="70px"></circle>
+                        </svg>
+                        <p class="pourcent"><span class="circle-span-value">50</span>%</p>
                     </div>
                 </div>
 
+                <div class="container-box slide-in">
+                    <p class="box-title">CSS</p>
+
+                    <div class="box">
+
+                        <svg>
+                            <circle data-value="90" class="circle-comp" cx="70px" cy="70px" r="70px"></circle>
+                        </svg>
+                        <p class="pourcent"><span class="circle-span-value">50</span>%</p>
+                    </div>
+                </div>
+                <div class="container-box slide-in">
+                    <p class="box-title">JavaScript</p>
+
+                    <div class="box">
+
+                        <svg>
+                            <circle data-value="80" class="circle-comp" cx="70px" cy="70px" r="70px"></circle>
+                        </svg>
+                        <p class="pourcent"><span class="circle-span-value">50</span>%</p>
+                    </div>
+                </div>
+            </div>
+            <p class="comp-title">Back</p>
+
+            <div class="comp-refonte-container">
+
+                <div class="container-box slide-in">
+                    <p class="box-title">PHP</p>
+
+                    <div class="box">
+
+                        <svg>
+                            <circle data-value="75" class="circle-comp" cx="70px" cy="70px" r="70px"></circle>
+                        </svg>
+                        <p class="pourcent"><span class="circle-span-value">50</span>%</p>
+                    </div>
+                </div>
+
+            </div>
+            <p class="comp-title">Autres</p>
+
+            <div class="comp-refonte-container">
+                <div class="container-box slide-in">
+                    <p class="box-title">C</p>
+
+                    <div class="box">
+
+                        <svg>
+                            <circle data-value="75" class="circle-comp" cx="70px" cy="70px" r="70px"></circle>
+                        </svg>
+                        <p class="pourcent"><span class="circle-span-value">50</span>%</p>
+                    </div>
+                </div>
+
+                <div class="container-box slide-in">
+                    <p class="box-title">LaTeX</p>
+
+                    <div class="box">
+
+                        <svg>
+                            <circle data-value="60" class="circle-comp" cx="70px" cy="70px" r="70px"></circle>
+                        </svg>
+                        <p class="pourcent"><span class="circle-span-value">50</span>%</p>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <p class="comp-title">En apprentissage</p>
+
+            <div class="comp-refonte-container">
+                <div class="container-box slide-in">
+                    <p class="box-title">Symfony</p>
+
+                    <div class="box">
+
+                        <svg>
+                            <circle data-value="65" class="circle-comp" cx="70px" cy="70px" r="70px"></circle>
+                        </svg>
+                        <p class="pourcent"><span class="circle-span-value">50</span>%</p>
+                    </div>
+                </div>
+                <div class="container-box slide-in">
+                    <p class="box-title">Json</p>
+
+                    <div class="box">
+
+                        <svg>
+                            <circle data-value="30" class="circle-comp" cx="70px" cy="70px" r="70px"></circle>
+                        </svg>
+                        <p class="pourcent"><span class="circle-span-value">50</span>%</p>
+                    </div>
+                </div>
+                <div class="container-box slide-in">
+                    <p class="box-title">Twig</p>
+
+                    <div class="box">
+
+                        <svg>
+                            <circle data-value="60" class="circle-comp" cx="70px" cy="70px" r="70px"></circle>
+                        </svg>
+                        <p class="pourcent"><span class="circle-span-value">50</span>%</p>
+                    </div>
+                </div>
             </div>
         </section>
         <section class="outils" id="outils">
             <div class="outils-container">
                 <h4 class="outils-titre">Les outils que j'utilise</h4>
+                <hr class="hr">
+
                 <div class="outils-list-c">
                     <ul>
                         <li class="outils-list">- Github</li>
@@ -157,8 +234,10 @@ if (isset($_POST['contact-titre']) && isset($_POST['contact-mail']) && isset($_P
                 </div>
             </div>
         </section>
-        <section class="projet">
+        <section class="projet" id="projet">
             <h4 class="projet-titre-global">Mes projets réalisés</h4>
+            <hr class="hr">
+
             <div class="galerie">
                 <div class="item" onclick="changedetails('1');">
                     <div class="projet-image protector">
@@ -174,12 +253,56 @@ if (isset($_POST['contact-titre']) && isset($_POST['contact-mail']) && isset($_P
                         <p class="projet-titre">Portfolio</p>
                     </div>
                 </div>
+                <div class="item" onclick="changedetails('3');">
+                    <div class="projet-image geographie">
+                    </div>
+                    <div class="projet-titre-container">
+                        <p class="projet-titre">Jeux géographiques</p>
+                    </div>
+                </div>
             </div>
-            
+
             <div id="projet-details" class="projet-details">
                 <h4 id="projet-titre" class="projet-details-titre"></h4>
                 <p id="projet-para" class="projet-details-para"></p>
-                <a id="projet-link"  href="" class="projet-details-lien" target="_blank"></a>
+                <a id="projet-link" href="" class="projet-details-lien" target="_blank"></a>
+            </div>
+        </section>
+        <section class="creation" id="creation">
+            <div class="creation-container">
+                <div class="creation-container-title">
+                    <h4 class="creation-titre-global">Mes créations</h4>
+                    <button id="filtres-creation" class="btn-filtres">Filtres</button>
+                </div>
+                <div class="creation-container-main">
+                    <div class="creation-container-galerie">
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="modal" id="modal">
+                <div class="modal-back"></div>
+                <div class="modal-container">
+                    <div class="modal-form">
+                        <label class="modal-label">
+                            <input type="checkbox" class="modal-input" value="html">Html
+                        </label>
+                        <label class="modal-label">
+                            <input type="checkbox" class="modal-input" value="css">Css
+                        </label>
+                        <label class="modal-label">
+                            <input type="checkbox" class="modal-input" value="js">Js
+                        </label>
+                        <label class="modal-label">
+                            <input type="checkbox" class="modal-input" value="c" >C
+                        </label>
+                        <label class="modal-label">
+                            <input type="checkbox" class="modal-input" value="php" >Php
+                        </label>
+                    </div>
+                    <button  id="modal-close">fermer</button>
+                </div>
             </div>
         </section>
         <section class="contact" id="contact">
@@ -190,6 +313,7 @@ if (isset($_POST['contact-titre']) && isset($_POST['contact-mail']) && isset($_P
                 <textarea type="text" name="contact-demande" required placeholder="Demande" rows="20" class="contact-demand form-input"></textarea>
                 <button type="submit" name="contact-bouton" class="contact-bouton  form-input">Envoyer</button>
             </form>
+
         </section>
 
         <?php
@@ -201,6 +325,8 @@ if (isset($_POST['contact-titre']) && isset($_POST['contact-mail']) && isset($_P
             </div>
         <?php } ?>
     </main>
+    <script src="./script.js"></script>
+
 </body>
 
 </html>
--- script.js
+++ script.js
@@ -1,18 +1,193 @@
 var details_projets = [
-    {Titre: "Protector 16", Description: "Dans le cadre du projet de création d'un site internet élégant et épuré, j'ai conçu le nouveau site web de l'entreprise Protector 16, située dans le 16ème arrondissement de Paris. L'objectif était de remplacer l'ancien site par une interface moderne et sophistiquée.<br><br>Le choix des couleurs principales, blanc (#F0F0F0) et noir (#333), apporte une sensation de fraîcheur tout en conservant un aspect visuel et chic. Ces teintes contrastées confèrent au site une esthétique contemporaine et professionnelle.<br><br>Pour renforcer l'attractivité visuelle du site, j\'ai veillé à sélectionner des polices élégantes et facilement lisibles, garantissant ainsi une expérience utilisateur agréable et intuitive.<br><br>En termes de fonctionnalités, le nouveau site propose une navigation fluide et intuitive, permettant aux visiteurs d'accéder rapidement aux informations pertinentes sur les produits et services de Protector 16. Des sections claires et bien structurées facilitent la recherche d'informations et mettent en valeur les éléments essentiels.<br><br>En outre, j'ai intégré des éléments interactifs et dynamiques, tels que des animations subtiles et des effets de transition élégants, pour donner une impression de modernité et de sophistication.<br><br>En somme, le nouveau site internet de Protector 16 incarne l'alliance parfaite entre l'élégance et la fonctionnalité, offrant ainsi aux utilisateurs une expérience en ligne agréable et mémorable.", Lien: "http://protector16.com" ,lien_affiche: "protector16.com"},
-    {Titre: "Portfolio personnel", Description: "Dans le but d'enrichir et de perfectionner mon expérience dans le domaine du développement web, j'ai créé un portfolio personnel mettant en valeur mes compétences et me présentant de manière professionnelle.<br><br>", Lien: "http://sinclair-moulager.fr",lien_affiche:"sinclair-moulager.fr"}
+  { Titre: "Protector 16", Description: "Dans le cadre du projet de création d'un site internet élégant et épuré, j'ai conçu le nouveau site web de l'entreprise Protector 16, située dans le 16ème arrondissement de Paris. L'objectif était de remplacer l'ancien site par une interface moderne et sophistiquée.<br><br>Le choix des couleurs principales, blanc (#F0F0F0) et noir (#333), apporte une sensation de fraîcheur tout en conservant un aspect visuel et chic. Ces teintes contrastées confèrent au site une esthétique contemporaine et professionnelle.<br><br>Pour renforcer l'attractivité visuelle du site, j\'ai veillé à sélectionner des polices élégantes et facilement lisibles, garantissant ainsi une expérience utilisateur agréable et intuitive.<br><br>En termes de fonctionnalités, le nouveau site propose une navigation fluide et intuitive, permettant aux visiteurs d'accéder rapidement aux informations pertinentes sur les produits et services de Protector 16. Des sections claires et bien structurées facilitent la recherche d'informations et mettent en valeur les éléments essentiels.<br><br>En outre, j'ai intégré des éléments interactifs et dynamiques, tels que des animations subtiles et des effets de transition élégants, pour donner une impression de modernité et de sophistication.<br><br>En somme, le nouveau site internet de Protector 16 incarne l'alliance parfaite entre l'élégance et la fonctionnalité, offrant ainsi aux utilisateurs une expérience en ligne agréable et mémorable.", Lien: "http://protector16.com", lien_affiche: "protector16.com" },
+  { Titre: "Portfolio personnel", Description: "Dans le but d'enrichir et de perfectionner mon expérience dans le domaine du développement web, j'ai créé un portfolio personnel mettant en valeur mes compétences et me présentant de manière professionnelle.<br><br>", Lien: "http://sinclair-moulager.fr", lien_affiche: "sinclair-moulager.fr" },
+  { Titre: "Hel Geographie", Description: "J'ai réalisé comme projet personnel, un site de jeux geographiques afin de me familiariser avec le format Svg et le javascript.", Lien: "http://hel-geographie.fr", lien_affiche: "hel-geographie.Fr" }
 ];
 
 
-function changedetails(nombre){
-    var titre = document.getElementById("projet-titre");
-    var desc = document.getElementById("projet-para");
-    var link = document.getElementById("projet-link");
-    var groupe = document.getElementById("projet-details");
-    titre.innerHTML = details_projets[nombre-1].Titre;
-    desc.innerHTML = details_projets[nombre-1].Description;
-    link.innerHTML = details_projets[nombre-1].lien_affiche;
-    link.href = details_projets[nombre-1].Lien;
-    groupe.style.display = "block";
-    groupe.scrollIntoView({		block: 'center',		behavior: 'smooth',		inline: 'nearest'	});	
+function changedetails(nombre) {
+  var titre = document.getElementById("projet-titre");
+  var desc = document.getElementById("projet-para");
+  var link = document.getElementById("projet-link");
+  var groupe = document.getElementById("projet-details");
+  titre.innerHTML = details_projets[nombre - 1].Titre;
+  desc.innerHTML = details_projets[nombre - 1].Description;
+  link.innerHTML = details_projets[nombre - 1].lien_affiche;
+  link.href = details_projets[nombre - 1].Lien;
+  groupe.style.display = "block";
+  groupe.scrollIntoView({ block: 'center', behavior: 'smooth', inline: 'nearest' });
 }
+
+
+
+const circleSlide = document.getElementsByClassName('slide-in');
+const styleSheet = document.styleSheets[0];
+const spanvalue = document.getElementsByClassName("circle-span-value");
+const circles = document.getElementsByClassName('circle-comp');
+
+window.addEventListener('scroll', () => {
+  const { scrollTop, clientHeight } = document.documentElement;
+
+  for (let i = 0; i < circleSlide.length; i++) {
+    const topElementToTopViewport = circleSlide[i].getBoundingClientRect().top;
+
+    if (scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.75) {
+      circleSlide[i].classList.add('active');
+    }
+  }
+
+  for (let i = 0; i < circles.length; i++) {
+    const circle = circles[i];
+    const prc = circle.getAttribute('data-value');
+    spanvalue[i].innerHTML = prc;
+
+    if (styleSheet) {
+      const parentContainer = circle.closest('.container-box');
+
+      if (parentContainer && parentContainer.classList.contains('active')) {
+        const keyframesRule = `@keyframes circle-animation-${i} {
+          to {
+            stroke-dashoffset: ${((100 - prc) * 440) / 100};
+          }
+        }`;
+
+        styleSheet.insertRule(keyframesRule, styleSheet.cssRules.length);
+        circle.style.animation = `circle-animation-${i} 2s forwards`;
+      }
+    }
+  }
+});
+
+var creation = [
+  { Titre: "Menu vertical", lien: "https://github.com/Alopsis/InclinedChoiceHalfAccordion", videosrc: "./video/2023-09-17_09-47-01.mkv", option: "html,css,js" },
+  { Titre: "Menu verticael", lien: "https://github.com/Alopsis/InclinedChoiceHalfAccordion", videosrc: "./video/2023-09-17_09-47-01.mkv", option: "html,php" }
+];
+let crea_affichage = [0];
+function makecreation(nombre) {
+  let container = document.createElement('div');
+  container.classList.add("creation-item");
+
+
+  let firstchild = document.createElement('div');
+  firstchild.classList.add('creation-image');
+  let video = document.createElement('video');
+  video.style.width = "500px";
+  video.style.height = "300px";
+  video.controls = true;
+
+  let src = document.createElement('source');
+  src.src = creation[nombre].videosrc;
+  src.type = "video/ogg";
+  video.appendChild(src);
+  firstchild.appendChild(video);
+
+
+  let secondchild = document.createElement('div');
+  secondchild.classList.add('creation-titre-container');
+  let titre = document.createElement('p');
+  titre.classList.add('creation-titre');
+  titre.innerHTML = creation[nombre].Titre;
+  secondchild.appendChild
+  let link = document.createElement('a');
+  link.classList.add('creation-button');
+  link.href = creation[nombre].lien;
+  link.innerHTML = "Voir sur github";
+  secondchild.appendChild(titre);
+  secondchild.appendChild(link);
+
+  container.appendChild(firstchild);
+  container.appendChild(secondchild);
+
+  container.dataset.opts = creation[nombre].option;
+  let corp = document.getElementsByClassName("creation-container-galerie")[0].appendChild(container);
+  crea_affichage[nombre] = 1;
+}
+
+function deletecreation(number) {
+  console.log("delete"+number);
+  let elem = document.getElementsByClassName('creation-item');
+  console.log(elem[0]);
+  console.log(elem[0].children[1].children[0].innerHTML);
+
+  for (let i = 0; i < elem.length; i++) {
+    if (elem[i].children[1].children[0].innerHTML == creation[number].Titre) {
+      console.log("REMOVE");
+      elem[i].remove();
+    }
+
+  }
+}
+function contains(a, obj) {
+  for (var i = 0; i < a.length; i++) {
+    if (a[i] === obj) {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+function estDansFiltre(nombre) {
+  let elem = creation[nombre];
+  let filtre = document.getElementsByClassName("modal-input");
+
+  for (i = 0; i < filtre.length; i++) {
+    let options = elem.option .split(',');
+    if (filtre[i].checked) {
+      console.log("Le filtre " + filtre[i].value + " est coché");
+      if (contains(options, filtre[i].value)) {
+        console.log("true");
+        return true;
+      }
+    }
+
+  }
+
+  return false;
+}
+function actualiseCreation() {
+  let creations = creation.length;
+  for (let i = 0; i < creations; i++) {
+    if (estDansFiltre(i)) {
+      if(crea_affichage[i] == 0){
+        makecreation(i);
+        crea_affichage[i] == 1;
+      }
+    } else {
+      console.log("cas 2");
+      if(crea_affichage[i] == 1){
+        deletecreation(i);
+        crea_affichage[i] = 0;
+      }
+    }
+
+  }
+}
+window.addEventListener("load", (event) => {
+  for (let i = 0; i < creation.length; i++) {
+    makecreation(i);
+    crea_affichage[i] = 1;
+  }
+  document.getElementById('filtres-creation').addEventListener('click', function (e) {
+    document.getElementById('modal').style.display = 'block'
+  });
+
+  document.getElementById('modal-close').addEventListener('click', function (e) {
+    document.getElementById('modal').style.display = 'none';
+    actualiseCreation();
+  })
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
